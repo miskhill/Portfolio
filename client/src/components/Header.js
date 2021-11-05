0642cadb685852490bfb0f278ffcbd8c3c74397d
garysmith <miskhill@hotmail.com>
2021-11-05 11:24:22 +0000

    fontsize header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,13 +9,13 @@ const Header = () =>  {
       <nav className="navbar black">
         <span className="navbar-toggle" id="js-navbar-toggle">
             <li className="link">
-              <Link to="/Main">About Me</Link>
+              <Link to="/Main" className="textLink">About Me</Link>
             </li>
             <li className="link">
-              <Link to="/Tech">Tech Stack</Link>
+              <Link to="/Tech" className="textLink">Tech Stack</Link>
             </li>
             <li className="link">
-              <Link to="/Project">My Projects</Link>
+              <Link to="/Project" className="textLink">My Projects</Link>
             </li>
         </span>
       </nav>
