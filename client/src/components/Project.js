d17c4835609b636ce0903f4a77ca061ad0c05d6f
garysmith <miskhill@hotmail.com>
2021-11-05 16:45:37 +0000

    card and images for projects

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -16,10 +16,10 @@ const Project = () => {
     
     <>
     <Carousel breakPoints={breakPoints}>
-      <Card number="Connect 4" />
-      <Card number="Movie Generator" />
-      <Card number="GCW Esports" />
-      <Card number="Hospitality Hub" />
+      <Card title="Connect 4" img="https://i.imgur.com/zAxT5UI.png" text="Classic game built with JavaScript, CSS & HTML"/>
+      <Card title="Movie Generator" img="https://i.imgur.com/dQuNrP5.png" text="Built with React, Express, JavaScript"/>
+      <Card title="GCW Esports" img="https://i.imgur.com/oAyjrg2.jpg" text="Built with React, Express, MongoDB & Mongoose"/>
+      <Card title="Hospitality Hub"img="https://i.imgur.com/l3crfLq.png" text="Built with Python, Django, Postgres, React & Express"/>
     </Carousel>
     </>
   )
--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Card = ({ number }) => {
+const Card = ({ title, img , text}) => {
   return (
     <>
     <div className="card">
-      {number}
+      <img className="card-img-top"src={img} alt="project img" width="200px" height="130px"></img>
+      <div className="card-body">
+      <h2 className="card-title">{title}</h2>
+      <div className="card-text">{text}</div>
+      <button className="btn btn-primary">Go to Project</button>
+      <button className="btn btn-primary">Go to ReadMe</button>
+      </div>
     </div>
     </>
   )
