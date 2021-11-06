de8954ab15b13f8886e23c6a0f6985a2907ca51c
garysmith <miskhill@hotmail.com>
2021-11-06 11:13:33 +0000

    about page images added and styling for text

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -6,21 +6,40 @@ import React from 'react'
     <div className="tech-col">
     <div className="contact-info">
     <h3 className="title">About Gary</h3>
-    <h4>Be fascinated not frustrated</h4>
-    <p>What an amazing mindset to have!! Whether learning a new Martial Art, trying a new game for the first time or picking up a new Tech stack I am always eager to learn as much as I can to demystify any issues I might face along my journey.</p>
-    <p>This desire to learn is what excited me about my journey into coding from my first evening on HTML (Thanks Codecademy) all the way through to joining General Assembly and embarking on their Software Engineering Immersive Bootcamp.</p>
+    <h4 className="abouth2">Be fascinated not frustrated</h4>
+    <p className="abouth2">What an amazing mindset to have!! Whether learning a new Martial Art, trying a new game for the first time or picking up a new Tech stack I am always eager to learn as much as I can to demystify any issues I might face along my journey.</p>
+    <p className="abouth2">This desire to learn is what excited me about my journey into coding from my first evening on HTML (Thanks Codecademy) all the way through to joining General Assembly and embarking on their Software Engineering Immersive Bootcamp.</p>
     <div className="card-wrap">
       <div className="card">
-      <h2>
+      <h2 className="abouth2">
       Cavaliers
     </h2>
-    <p>I breed and show Cavalier King Charles Spaniels and as of writing this I have 6 puppies who are all healthy and full of energy! I have been to Crufts Dog Show with my dogs.</p>
+    <p className="abouth2">I breed and show Cavalier King Charles Spaniels and as of writing this I have 6 puppies who are all healthy and full of energy!</p>
+    <div className="photo-wrap">
+    <img src="https://i.imgur.com/KfjsEHn.jpg" alt="Cav" className="photo"></img>
+    <img src="https://i.imgur.com/DfwTRB8.jpg" alt="Cav" className="photo"></img>
+    <img src="https://i.imgur.com/GKttS6r.jpg" alt="Cav" className="photo"></img>
+    </div>
       </div>
       <div className="card">
-      <h2>
+      <h2 className="abouth2">
       Kickboxing Coach
     </h2>
-    <p>I coach Kickboxing and hold a Black Belt as well as in Tae Kwon Do.</p>
+    <p className="abouth2">I train and coach Kickboxing and Tae Kwon Do. I also go to the gym regularly.</p>
+    <div className="photo-wrap">
+    <img src="https://i.imgur.com/h2hdGhL.jpg" alt="kickboxing" className="photo"></img>
+    </div>
+      </div>
+      <div className="card">
+      <h2 className="abouth2">
+      Gaming
+    </h2>
+    <p className="abouth2">I enjoy Starcraft 2, World of Warcraft and Football Manager when it is time to relax!</p>
+    <div className="photo-wrap">
+    <img src="https://i.imgur.com/KfjsEHn.jpg" alt="Cav" className="photo"></img>
+    <img src="https://i.imgur.com/DfwTRB8.jpg" alt="Cav" className="photo"></img>
+    <img src="https://i.imgur.com/GKttS6r.jpg" alt="Cav" className="photo"></img>
+    </div>
       </div>
     </div>
     </div>
