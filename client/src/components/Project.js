32d03b861c25b941eea95cffb420842c092d198d
garysmith <miskhill@hotmail.com>
2021-11-08 12:17:49 +0000

    linking from projects

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -16,8 +16,8 @@ const Project = () => {
     
     <>
     <Carousel breakPoints={breakPoints}>
-      <Card title="Connect 4" img="https://i.imgur.com/zAxT5UI.png" text="Classic game built with JavaScript, CSS & HTML" id="connect4"/>
-      <Card title="Movie Generator" img="https://i.imgur.com/HjjLtGN.jpg" text="Built with React, Express, JavaScript"/>
+      <Card title="Connect 4" href="https://github.com/miskhill/Project1" img="https://i.imgur.com/zAxT5UI.png" text="Classic game built with JavaScript, CSS & HTML" id="connect4"/>
+      <Card title="Movie Generator" img="https://i.imgur.com/HjjLtGN.jpg" text="Built with React, Express, JavaScript & Bootstrap"/>
       <Card title="GCW Esports" img="https://i.imgur.com/oAyjrg2.jpg" text="Built with React, Express, MongoDB & Mongoose"/>
       <Card title="Hospitality Hub"img="https://i.imgur.com/l3crfLq.png" text="Built with Python, Django, Postgres, React & Express"/>
     </Carousel>
--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const Card = ({ title, img , text}) => {
+const Card = ({ title, img , text, href }) => {
+
+// function handleClick() {
+//   console.log('clicked')
+// }
+
   return (
     <>
     <div className="card">
@@ -9,7 +14,7 @@ const Card = ({ title, img , text}) => {
       <h2 className="card-title">{title}</h2>
       <div className="card-text">{text}</div>
       <div className="button-div">
-      <button className="btn btn-primary">Visit Project</button>
+      <button className="btn btn-primary"><a href={href} target="_blank" rel="nonreferrer">Visit Project</a></button>
       <button className="btn btn-primary">Visit ReadMe</button>
       </div>
       </div>
