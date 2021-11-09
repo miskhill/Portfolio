7bf67447a3043816984568fc657b9767d15f0bed
garysmith <miskhill@hotmail.com>
2021-11-09 12:37:19 +0000

    media query

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -17,17 +17,17 @@ const Project = () => {
     <>
     <div className="tech-col">
     <div className="contact-info">
-    <h3 className="title">Projects</h3>
+    <h3 className="title" id="project-title">Projects</h3>
     </div>
     </div>
     
     
 
     <Carousel breakPoints={breakPoints}>
-      <Card title="Connect 4" href="https://github.com/miskhill/Project1" img="https://i.imgur.com/zAxT5UI.png" text="Classic game built with JavaScript, CSS & HTML" id="connect4"/>
-      <Card title="Movie Generator" img="https://i.imgur.com/HjjLtGN.jpg" text="Built with React, Express, JavaScript & Bootstrap"/>
-      <Card title="GCW Esports" img="https://i.imgur.com/oAyjrg2.jpg" text="Built with React, Express, MongoDB & Mongoose"/>
-      <Card title="Hospitality Hub"img="https://i.imgur.com/l3crfLq.png" text="Built with Python, Django, Postgres, React & Express"/>
+      <Card title="Connect 4" href="https://github.com/miskhill/Project1" href2="https://github.com/miskhill/Project1#readme" img="https://i.imgur.com/zAxT5UI.png" text="Classic game built with JavaScript, CSS & HTML" id="connect4"/>
+      <Card title="Movie Generator" href="https://movieproject2.netlify.app" href2="https://github.com/miskhill/Project-2#readme" img="https://i.imgur.com/HjjLtGN.jpg" text="Built with React, Express, JavaScript & Bootstrap"/>
+      <Card title="GCW Esports" href="https://github.com/CosmasC128/Project-3" href2="https://github.com/CosmasC128/Project-3#readme" img="https://i.imgur.com/oAyjrg2.jpg" text="Built with React, Express, MongoDB & Mongoose"/>
+      <Card title="Hospitality Hub" href="https://github.com/CosmasC128/Project-4/tree/development" href2="https://github.com/miskhill/Project1#readme" img="https://i.imgur.com/l3crfLq.png" text="Built with Python, Django, Postgres, React & Express"/>
     </Carousel>
     </>
   )
--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Card = ({ title, img , text, href }) => {
+const Card = ({ title, img , text, href, href2 }) => {
 
 // function handleClick() {
 //   console.log('clicked')
@@ -9,13 +9,15 @@ const Card = ({ title, img , text, href }) => {
   return (
     <>
     <div className="card">
+      <div>
       <img className="card-img-top"src={img} alt="project img" width="200px" height="130px"></img>
+      </div>
       <div className="card-body">
       <h2 className="card-title">{title}</h2>
       <div className="card-text">{text}</div>
       <div className="button-div">
       <button className="btn btn-primary"><a href={href} target="_blank" rel="nonreferrer">Visit Project</a></button>
-      <button className="btn btn-primary">Visit ReadMe</button>
+      <button className="btn btn-primary"><a href={href2} target="_blank" rel="nonreferrer">Visit ReadMe</a></button>
       </div>
       </div>
     </div>
