4c08eaf2988b5986f99c1ed5f33c3e090a94d3c4
garysmith <miskhill@hotmail.com>
2021-11-11 15:07:58 +0000

    media query

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -10,39 +10,39 @@ import React from 'react'
     <img src="https://i.imgur.com/iLcgqBD.jpg" alt="Gary" className="gary-photo"></img>
     </div>
     <h4 className="abouth2">Be fascinated not frustrated</h4>
+    <div className="💬">
     <p className="abouth2">What an amazing mindset to have!! Whether learning a new Martial Art, trying a new game for the first time or picking up a new Tech stack I am always eager to learn as much as I can to demystify any issues I might face along my journey.</p>
     <p className="abouth2">This desire to learn is what excited me about my journey into coding from my first evening on HTML (Thanks Codecademy) all the way through to joining General Assembly and embarking on their Software Engineering Immersive Bootcamp.</p>
+    </div>
     <div className="card-wrap">
-      <div className="card">
-      <h2 className="abouth2">
-      Cavaliers
-    </h2>
+      <div className="💳">
+      <h2 className="abouth2">Cavaliers</h2>
     <p className="abouth2">I breed and show Cavalier King Charles Spaniels and as of writing this I have 6 puppies who are all healthy and full of energy!</p>
     <div className="photo-wrap">
-    <img src="https://i.imgur.com/KfjsEHn.jpg" alt="Cav" className="photo"></img>
-    <img src="https://i.imgur.com/DfwTRB8.jpg" alt="Cav" className="photo"></img>
-    <img src="https://i.imgur.com/GKttS6r.jpg" alt="Cav" className="photo"></img>
+    <img id="img1" src="https://i.imgur.com/KfjsEHn.jpg" alt="Cav" className="photo"></img>
+    <img id="img2" src="https://i.imgur.com/DfwTRB8.jpg" alt="Cav" className="photo"></img>
+    <img id="img3" src="https://i.imgur.com/GKttS6r.jpg" alt="Cav" className="photo"></img>
     </div>
       </div>
-      <div className="card">
+      <div className="💳">
       <h2 className="abouth2">
       Kickboxing Coach
     </h2>
     <p className="abouth2">I train and coach Kickboxing and Tae Kwon Do. I also go to the gym regularly.</p>
     <div className="photo-wrap">
-    <img src="https://i.imgur.com/h2hdGhL.jpg" alt="kickboxing" className="photo"></img>
-    <img src="https://i.imgur.com/woag5Tf.jpg" alt="kickboxing" className="photo"></img>
+    <img id="img4" src="https://i.imgur.com/h2hdGhL.jpg" alt="kickboxing" className="photo"></img>
+    <img id="img5" src="https://i.imgur.com/woag5Tf.jpg" alt="kickboxing" className="photo"></img>
     </div>
       </div>
-      <div className="card">
+      <div className="💳">
       <h2 className="abouth2">
       Gaming
     </h2>
     <p className="abouth2">I enjoy Starcraft 2, World of Warcraft and Football Manager when it is time to relax!</p>
     <div className="photo-wrap">
-    <img src="https://i.imgur.com/KfjsEHn.jpg" alt="Cav" className="photo"></img>
-    <img src="https://i.imgur.com/DfwTRB8.jpg" alt="Cav" className="photo"></img>
-    <img src="https://i.imgur.com/GKttS6r.jpg" alt="Cav" className="photo"></img>
+    <img id="img6" src="https://i.imgur.com/1FWTGCY.jpg" alt="Cav" className="photo"></img>
+    <img id="img7" src="https://i.imgur.com/fb9RHv5.jpg" alt="Cav" className="photo"></img>
+    <img id="img8" src="https://i.imgur.com/yuRvb6V.jpg" alt="Cav" className="photo"></img>
     </div>
       </div>
     </div>
