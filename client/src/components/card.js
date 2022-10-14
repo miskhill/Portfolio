5ce6100f62ef385a31dfcf434cf151e614e5fe5c
Gary Smith <80961839+miskhill@users.noreply.github.com>
2022-10-14 14:46:53 +0100

    card edits

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -15,7 +15,6 @@ const Card = ({ title, img , text, href, href2 }) => {
       <div className="button-div">
       <button className="btn btn-primary"><a href={href} target="_blank" rel="noreferrer">Visit Project</a></button>
       <button className="btn btn-primary"><a href={href2} target="_blank" rel="noreferrer">Visit ReadMe</a></button>
-      <i>i</i>
       </div>
       </div>
     </div>
